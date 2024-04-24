403e9881c6de8e9b86c6d4eb8c128eeab6117638
Artur <99425670+ArturMorenoBertran@users.noreply.github.com>
2024-04-24 21:38:44 +0200

    delete errors

--- IAM_Users/createUsers.tf
+++ IAM_Users/createUsers.tf
@@ -1,4 +1,4 @@
-provider "aws" {
+rovider "aws" {
   region = "eu-west-3"
 }
 
@@ -11,26 +11,23 @@ variable "users" {
 resource "aws_iam_user" "create_users" {
   for_each = var.users
 
-  name = "user-${each.value}"
+  name = each.value
 }
 
 
 # Obtain specific user arn's
 output "arn_user" {
-  value = aws_iam_user.users["artur"].arn
+  value = aws_iam_user.create_users["artur"].arn
 }
+
 
 # Obtain all user arn's
 output "arn_all_users" {
-  value = [for user in aws_iam_user.users : user.arn]
+  value = [for user in aws_iam_user.create_users : user.arn]
 }
 
-# Obtain all user names with splat expresion
-output "arn_all_user_names" {
-  value = aws_iam_user.users[*].name
-}
 
 # Map user name with arn
-output "arn_all_users" {
-  value = [for user in aws_iam_user.users: user.name => user.arn]
+output "map_users_arn" {
+  value = {for user in aws_iam_user.create_users: user.name => user.arn}
 }
